21f8424bcb601754c4baf03c8d8f5a618c27f2c6
salmaarayyan <salmaarayyan@gmail.com>
2025-10-23 12:28:40 +0700

    Merevisi MainActivity

--- app/src/main/java/com/example/questadvancelayout_082/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_082/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_082Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
