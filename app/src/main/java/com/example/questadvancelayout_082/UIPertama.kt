1e593d2db9eeb3c814d71f06ff8a5632fe14f9f9
salmaarayyan <salmaarayyan@gmail.com>
2025-10-23 11:35:14 +0700

    Membuat code untuk memanggil text id prodi

--- app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
@@ -1,7 +1,9 @@
 package com.example.questadvancelayout_082
 
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -10,5 +12,10 @@ fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+
+    }
 }
