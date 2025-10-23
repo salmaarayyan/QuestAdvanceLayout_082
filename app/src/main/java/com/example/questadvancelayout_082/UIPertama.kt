ebb3dc449e44efd702e31103a6b67450fd403b4c
salmaarayyan <salmaarayyan@gmail.com>
2025-10-23 11:32:20 +0700

    Membuat function dan column yang dimodifikasi

--- app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
@@ -1,1 +1,14 @@
 package com.example.questadvancelayout_082
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
