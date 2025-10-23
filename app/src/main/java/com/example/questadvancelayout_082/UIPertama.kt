0743558a4caa10800e3ce44dbc306f5236e5af98
salmaarayyan <salmaarayyan@gmail.com>
2025-10-23 11:52:03 +0700

    Merevisi code yang masih error dan membuat card yang dimodifikasi

--- app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
@@ -1,24 +1,45 @@
 package com.example.questadvancelayout_082
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
-@composable
+@Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier = Modifier.padding(top=100.dp)
+    Column(modifier = Modifier
+        .padding(top = 100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(id = R.string.prodi),
+        Text(stringResource(R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold)
-        Text(text = stringResource(id= R.string.univ),
+        Text(stringResource(R.string.univ),
             fontSize = 22.sp)
-        Spacer(modifier = Modifier.height(height=25.dp))
+        Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){
+
+        }
     }
 }
