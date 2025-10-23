f55d77a121ad4081b1e6be262e3562d93e6c942b
salmaarayyan <salmaarayyan@gmail.com>
2025-10-23 12:12:41 +0700

    Membuat box

--- app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_082/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_082
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -70,6 +71,8 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Box(
 
+        )
     }
 }
